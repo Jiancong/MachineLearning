5449a0ece4c3c9d4e37da914e4f708cc13108c9a
jiancongxie <jianc.xie@gmail.com>
2018-11-12 21:26:19 +0800

    Add normal equation method to theta.

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -87,13 +87,31 @@ num_iters = 400;
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
 
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+[theta, J_history1] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+
+alpha = 0.05;
+theta = zeros(3, 1);
+[theta, J_history2] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+alpha = 0.1;
+theta = zeros(3, 1);
+[theta, J_history3] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+alpha = 1;
+theta = zeros(3, 1);
+[theta, J_history4] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
 % Plot the convergence graph
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+plot(1:numel(J_history1), J_history1, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
+
+hold on;
+plot(1:numel(J_history2), J_history2, 'xr', 'LineWidth', 2);
+plot(1:numel(J_history3), J_history3, '+k', 'LineWidth', 2);
+plot(1:numel(J_history4), J_history4, '+b', 'LineWidth', 2);
 
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
@@ -155,7 +173,9 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
+temp = [1 1650 3];
 
+price = temp * theta;
 
 % ============================================================
 
